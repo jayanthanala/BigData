a1c538fbc2954a8a0d2708fdf7290687b17bbfcd
Jayanth Anala <javaoneapl@gmail.com>
2023-12-06 20:08:37 -0500

    Add: V2 Code

--- main.py
+++ main.py
@@ -5,15 +5,21 @@ import datetime
 from pathlib import Path
 import os
 import pandas as pd
-import numpy as np
 import pyarrow.parquet as pq
 import pyarrow as pa
 import sys
-import nltk
+import numpy as np
+import re
 from nltk.tokenize import word_tokenize
+import nltk
+
+from IPython.core.display import HTML
+display(HTML("<style>pre { white-space: pre !important; }</style>"))
+
 
 from pyspark import SparkContext, SparkConf
-from pyspark.sql.functions import format_string,col, when, coalesce
+from pyspark.sql.functions import format_string,col, when, coalesce, count, isnan,lit, udf
+from pyspark.sql.types import StringType
 cf = SparkConf()
 cf.set("spark.submit.deployMode","client")
 sc = SparkContext.getOrCreate(cf)
@@ -43,19 +49,92 @@ df2 = df.select([col(c).alias(
     
 df3 = spark.read.schema(df2.schema).parquet(*file_path_list)
 df3.printSchema()
-df3.show(1,False)
-df3.count()
+df4 = df3.dropDuplicates() ## no duplicates 
 
-df4 = df3.dropDuplicates()
-df4.count()
+## Cleaning and Metrics of the dataset
+labels=["a real animal","an animal body part"]
+df5 = df4.filter(df4.label_product.isin(labels))
+df5.limit(5)
 
-animals = spark.read.format('csv').options(header='true',inferschema='true').load('/user/ja4874_nyu_edu/datasets/03_gbif_common_names.csv')
-animalsArray = animals.select("gbif_common_name","English name of Language","French name of Language","German name of Language").toPandas().values.reshape(-1)
+df5.select('url').distinct().count()
+df5.select('domain').distinct().count()
+
+df5.groupBy(df5.seller)
+df5.limit(5)
+
+df5.select('location').distinct().count()
+df5.groupBy("location").agg({"location":"count"}).show()
+
+df5.groupBy("country").agg({"country":"count"}).show()
+
+df5.summary("count").show() ## shows count of all the records wrt columns
+
+df5.select([count(when(col(c).contains('None') | \
+                            col(c).contains('null') | \
+                            (col(c) == '' ) | \
+                            col(c).isNull() | \
+                            isnan(c), c 
+                           )).alias(c)
+                    for c in df5.columns]).show() ## shows count of non-null value records
+
+## Datasets Union (external) (FOR EXPERIMENTING)
+cols=('species','subspecies','id_kingdom','id_phylum','id_class','id_order','id_family','id_genus','id_species','id_subspecies','match_type','gbif_name','gbif_rank','kingdom','phylum','class','order','family','genus')
+parFile1.unionByName(parFile2, allowMissingColumns=True).drop(*cols).dropna().show(200)
+
+parFile1.unionByName(parFile2, allowMissingColumns=True).drop(*cols).dropna().join(parFile3, 'gbif_id').join(parFile5, ['standardized_use_id','standardized_use_type']).dropna().show()
+
+##DS1
+# csvFile = spark.read.format('csv').options(header='true',inferschema='true').load('datasets/01_taxa_use_combos.csv')
+# csvFile.write.parquet("01_taxa_use_combos.csv.parquet")
+parFile1 = spark.read.parquet('01_taxa_use_combos.csv.parquet')
+parFile1.printSchema()
+parFile1.summary('count').show()
+parFile1.show()
+
+##DS2
+# csvFile = spark.read.format('csv').options(header='true',inferschema='true').load('datasets/02_gbif_taxonomic_key.csv')
+# csvFile.write.parquet("datasets/02_gbif_taxonomic_key.parquet")
+parFile2 = spark.read.parquet('datasets/02_gbif_taxonomic_key.parquet')
+parFile2.printSchema()
+parFile2.summary('count').show()
+parFile2.show()
+
+#DS3
+# csvFile = spark.read.format('csv').options(header='true',inferschema='true').load('datasets/03_gbif_common_names.csv')
+# csvFile.where(col('English name of language')=='English').drop('ISO 639-2 Code','ISO 639-1 Code','French name of Language','German name of Language','English name of Language').write.parquet("datasets/03_gbif_common_names.parquet")
+parFile3 = spark.read.parquet('datasets/03_gbif_common_names.parquet')
+parFile3.printSchema()
+parFile3.summary('count').show()
+parFile3.show()
+
+# Joining the datasets
+
+parFile4=parFile2.join(parFile3, parFile2.id_species ==  parFile3.gbif_id)
+#id_* can be removed
+# count:15322
+parFile5=parFile1.join(parFile4, parFile1.db_taxa_name ==  parFile4.db_taxa_name)
+parFile5.show()
+
+
+## Function to Extract the animal names from title
+#animals = spark.read.format('csv').options(header='true',inferschema='true').load('/user/ja4874_nyu_edu/datasets/03_gbif_common_names.csv')
+animalsArray = parFile3.select("gbif_common_name").toPandas().values.reshape(-1)
 animalsArrayUnique = np.unique(animalsArray)
+# animalsArrayUnique = list(map(lambda x: x.lower(), animalsArrayUnique))
+print(animalsArrayUnique)
 
-//fix this
-def extract_animals(text):
-    tokenized_text = word_tokenize(text.lower())
-    return [word for word in tokenized_text if word in animalsArrayUnique]
 
-df['animals'] = df['text'].apply(extract_animals)
+def extract_animals(title):
+    matches = re.findall(fr'\b(?:{"|".join(animalsArrayUnique)})\b', title, flags=re.IGNORECASE)
+    return matches[0] if matches else None
+
+extract_animal_udf = udf(extract_animals, StringType())
+
+##..... run only once and use the saved parquet file.....
+df6 = df5.withColumn("animal_names", extract_animal_udf(col("title")))
+df6.write.parquet("datasets/df6_animals.parquet") 
+## ........................................................
+df6.limit(5)
+
+
+## Remaining (Union the professor dataset and external dataset for analysis)
